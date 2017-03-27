fea0d6a1eba9857b16340f267a216c26aadd25c1
ohgam <ohgam@DESKTOP-L0LFUIH>
2017-03-27 14:04:07 +0900

    production environment setting

---
+++ readinglist-rest-client/WebContent/js/properties.js
@@ -0,0 +1,7 @@
+//web_admin_url="http://localhost:8000";
+//rest_server_url="http://localhost:8001";
+//rest_client_url="http://localhost:8002";
+
+web_admin_url="http://13.124.89.247:8000";
+rest_server_url="http://13.124.89.247:8001";
+rest_client_url="http://13.124.89.247:8002";
