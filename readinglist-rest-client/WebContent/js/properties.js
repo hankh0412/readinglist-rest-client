c73dba55fed7d465ac3322a2948923125a96b78c
ohgamja3 <ohgamja3@gmail.com>
2018-03-02 18:00:06 +0900

    n/a

--- readinglist-rest-client/WebContent/js/properties.js
+++ readinglist-rest-client/WebContent/js/properties.js
@@ -1,13 +1,14 @@
-/*web_admin_url="http://localhost:8000";
+web_admin_url="http://localhost:8000";
 rest_server_url="http://localhost:8001";
 rest_client_url="http://localhost:8002";
 books_mgmt_url="http://localhost:8003";
-*/
 
+/*
 web_admin_url="http://192.168.0.8:8000";
 rest_server_url="http://192.168.0.8:8001";
 rest_client_url="http://192.168.0.8:8002";
 books_mgmt_url="http://192.168.0.8:8003";
+*/
 
 //web_admin_url="http://13.124.89.247:8000";
 //rest_server_url="http://13.124.89.247:8001";
